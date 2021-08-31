d3881b5de76a8ae1c4c1589a558db2cbeb27208e
niles <niles@LAPTOP-3K24CQ1R>
2021-09-01 00:18:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleAppPalindromeCharacterArray
+{
+    class PalCharacter
+    {
+
+
+        internal void PalCharArray(char[] name)
+        {
+            bool palindrome = true;
+            int i, j, num;
+            num = name.Length;
+            for (i = 0, j = num - 1; i < num / 2; i++, j--)
+            {
+                if (name[i] != name[j])
+                {
+                    palindrome = false;
+                    break;
+                }
+            }
+            Console.WriteLine("String : " + palindrome);
+        }
+    }    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string : ");
+            char[] name = new char[10];
+            name = Console.ReadLine().ToCharArray();
+            new PalCharacter().PalCharArray(name);
+        }
+    }
+}
